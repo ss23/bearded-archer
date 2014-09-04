f59932fd3248f2f707db2ccbfa245f4e79d26d7d
Stephen Shkardoon <ss23@ss23.geek.nz>
2014-09-05 00:42:05 +1200

    Another attempt and making it not-interactive

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
-apt-get -qq update
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y -qq update
 
 # Set required configuration, e.g. MariaDB
 mysqlpassword="__MYSQLRANDOMPASSWORD__"
 echo mariadb-server-5.5 mysql-server/root_password password "$mysqlpassword" | debconf-set-selections
 echo mariadb-server-5.5 mysql-server/root_password_again password "$mysqlpassword" | debconf-set-selections
 
-apt-get -qq upgrade 
-apt-get -qq dist-upgrade
-apt-get -qq install apache2 php5 libapache2-mod-php5 mariadb-server php5-mysql git php5-gd php5-tidy php5-curl s3cmd
-apt-get -qq autoremove
+apt-get -y -qq upgrade 
+apt-get -y -qq dist-upgrade
+apt-get -y -qq install apache2 php5 libapache2-mod-php5 mariadb-server php5-mysql git php5-gd php5-tidy php5-curl s3cmd
+apt-get -y -qq autoremove
