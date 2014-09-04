83f36ad1049dd38710db3a2b2e1741e1d6684c0f
Stephen Shkardoon <ss23@ss23.geek.nz>
2014-09-05 00:34:19 +1200

    Add MariaDB configuration

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 apt-get -qq update
+
+# Set required configuration, e.g. MariaDB
+mysqlpassword="__MYSQLRANDOMPASSWORD__"
+echo mariadb-server-5.5 mysql-server/root_password password "$mysqlpassword" | debconf-set-selections
+echo mariadb-server-5.5 mysql-server/root_password_again password "$mysqlpassword" | debconf-set-selections
+
 apt-get -qq upgrade 
 apt-get -qq dist-upgrade
 apt-get -qq install apache2 php5 libapache2-mod-php5 mariadb-server php5-mysql git php5-gd php5-tidy php5-curl s3cmd
